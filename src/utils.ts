3e4de4854d79cda44e05a3279d925dc2f4acc070
Johanna Räihälä <johanna.raihala@gmail.com>
2021-06-08 10:59:09 +0200

    add shuffle array

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,4 @@
 // to randomize the answers to the questions
+// array is spread with the spread operator and a new array will be created
+export const shuffleArray = (array: any[]) =>
+  [...array].sort(() => Math.random() - 0.5);
